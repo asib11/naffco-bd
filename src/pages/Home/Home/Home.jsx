9bfe6166f042b7853bc3f851c9f92dcad359e23f
asib ahmed <ahmedasib1@gmail.com>
2024-09-02 22:50:31 +0600

    setup naffco project

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import AboutUs from '../AboutUs/AboutUs';
+
+const Home = () => {
+    return (
+        <div>
+            <AboutUs></AboutUs>
+        </div>
+    );
+};
+
+export default Home;
